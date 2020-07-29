1b911142a94c75f9a4a7790c504fbf16003baec5
lukazivanovic <sdluka@hotmail.com>
2020-07-29 16:58:20 +0200

    calculator first

---
+++ script.js
@@ -0,0 +1,31 @@
+function showRes(){
+    var battleResult = document.getElementById("display").value;
+    var calculate = eval(battleResult);
+    document.getElementById("result").innerHTML = calculate;
+}
+
+// Get the modal
+var modal = document.getElementById("throwModal");
+
+// Get the button that opens the modal
+var throwbtn = document.getElementById("throwBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+throwbtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
