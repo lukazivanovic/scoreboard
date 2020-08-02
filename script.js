f9e7e23289b5bd2e265877dce774fe1e54374e2f
lukazivanovic <sdluka@hotmail.com>
2020-08-02 12:27:03 +0200

    bootstrap

--- script.js
+++ script.js
@@ -20,42 +20,6 @@ function startGame(){
   modal1.style.display = "none";
 }
 
-/** CALCULATION MODAL */
-// Get the modal
-var modal = document.getElementById("throwModal");
-// Get the button that opens the modal
-var throwbtn = document.getElementById("throwBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-throwbtn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-/** START MODAL */
-var modal1 = document.getElementById("startModal");
-var newbtn = document.getElementById("newgameBtn");
 window.onload = function() {
-  modal1.style.display = "block";
-}
-newbtn.onclick = function() {
-  modal1.style.display = "block";
-}
-span.onclick = function() {
-  modal1.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
+  $('#startModal').modal('show');
 }
