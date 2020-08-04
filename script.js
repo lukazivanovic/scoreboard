56464b379edc8786111c54d7c020127f86755843
lukazivanovic <sdluka@hotmail.com>
2020-08-04 03:22:10 +0200

    clock, infobox, first calculator results

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+window.onload = function() {
+  $('#startModal').modal('show');
+}
+
 function backToHomePage(){
   window.open("index.html","_self");
 }
 
-function showRes(){
-  var battleResult = document.getElementById("display").value;
-  var calculate = eval(battleResult);
-  document.getElementById("result").innerHTML = calculate;
+function showRes() {
+  event.preventDefault();
+  var pl1 = eval(document.getElementById("player1number").innerHTML);
+  var pl2 = eval(document.getElementById("player2number").innerHTML);
+  var infobox = document.getElementById("infobox");
+  var battleResult = document.forms["calculator"]["display"].value;
+  var calculate = eval(battleResult); 
+  if(calculate > 0) pl2 = pl2 - calculate;
+  else if (calculate < 0) pl1 = pl1 + calculate;
+  document.getElementById("player1number").innerHTML = pl1; 
+  document.getElementById("player2number").innerHTML = pl2; 
+  infobox.innerHTML = "Previous move: " + calculate;
+  $('#battleModal').modal('hide');
+  document.forms["calculator"]["display"].value = "";
 }
 
 function startGame(){
@@ -21,9 +35,6 @@ function startGame(){
   var pl2nameGame = document.getElementById("pl2name").value;
   var pl1numGame = document.getElementById("player1num").value;
   var pl2numGame = document.getElementById("player2num").value;
-  modal1.style.display = "none";
+  modal1.style.display = "none"; 
 }
 
-window.onload = function() {
-  $('#startModal').modal('show');
-}
