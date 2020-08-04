03741171fb56434aed199c6bc1d480366293cb08
lukazivanovic <sdluka@hotmail.com>
2020-08-04 16:58:01 +0200

    code

--- script.js
+++ script.js
@@ -6,35 +6,41 @@ function backToHomePage(){
   window.open("index.html","_self");
 }
 
-function showRes() {
-  event.preventDefault();
-  var pl1 = eval(document.getElementById("player1number").innerHTML);
-  var pl2 = eval(document.getElementById("player2number").innerHTML);
-  var infobox = document.getElementById("infobox");
-  var battleResult = document.forms["calculator"]["display"].value;
-  var calculate = eval(battleResult); 
-  if(calculate > 0) pl2 = pl2 - calculate;
-  else if (calculate < 0) pl1 = pl1 + calculate;
-  document.getElementById("player1number").innerHTML = pl1; 
-  document.getElementById("player2number").innerHTML = pl2; 
-  infobox.innerHTML = "Previous move: " + calculate;
-  $('#battleModal').modal('hide');
-  document.forms["calculator"]["display"].value = "";
-}
+var pl1name = document.getElementById("pl1name");
+var pl2name = document.getElementById("pl2name");
+var pl1num = document.getElementById("pl1num");
+var pl2num = document.getElementById("pl2num");
 
 function startGame(){
-  var pl1name = document.getElementById("pl1name").value;
-  var pl2name = document.getElementById("pl2name").value;
-  var pl1num = document.getElementById("pl1num").value;
-  var pl2num = document.getElementById("pl2num").value;
-  document.getElementById("player1name").innerHTML = pl1name;
-  document.getElementById("player2name").innerHTML = pl2name;
-  document.getElementById("player1number").innerHTML = pl1num;
-  document.getElementById("player2number").innerHTML = pl2num;
-  var pl1nameGame = document.getElementById("pl1name").value;
-  var pl2nameGame = document.getElementById("pl2name").value;
-  var pl1numGame = document.getElementById("player1num").value;
-  var pl2numGame = document.getElementById("player2num").value;
-  modal1.style.display = "none"; 
+  if(pl1name.value == "") document.getElementById("player1name").innerHTML = "Player 1";
+  else document.getElementById("player1name").innerHTML = pl1name.value;
+  if(pl2name.value == "") document.getElementById("player2name").innerHTML = "Player 2";
+  else document.getElementById("player2name").innerHTML = pl2name.value;
+
+  document.getElementById("player1number").innerHTML = pl1num.value;
+  document.getElementById("player2number").innerHTML = pl2num.value;
+  
+  $('#startModal').modal('hide'); 
 }
+
+var player1num = eval(document.getElementById("player1number").innerHTML);
+var player2num = eval(document.getElementById("player2number").innerHTML);
+var infobox = document.getElementById("infobox");
+var historyText = document.getElementById("historyText");
+
+function showRes() {
+  event.preventDefault();
+  var battleResult = document.forms["calculator"]["display"].value;
+  var calculate = eval(battleResult); 
+  if(calculate > 0) player2num -= calculate;
+  else if (calculate < 0) player1num += calculate;
+  player1number.innerHTML = player1num; 
+  player2number.innerHTML = player2num; 
+  infobox.innerHTML = "Previous move: " + calculate;
+  historyText.innerHTML += "[time], " + document.getElementById("player1name").innerHTML + " attacks " + document.getElementById("player2name").innerHTML + ", " + calculate + " damage, " + "(" + battleResult + ") <br>";
+  $('#battleModal').modal('hide');
+  battleResult = "";
+}
+
+
 
